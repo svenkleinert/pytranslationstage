b9f409989d81156e8a1cb5a9d766a769e68b322a
Sven Kleinert <kleinert@iqo.uni-hannover.de>
2021-05-07 14:19:41 +0200

    GUI applications added for simple translationstage control

---
+++ pytranslationstage/main_wedge_gui.py
@@ -0,0 +1,17 @@
+
+import sys
+
+def main():
+    from  gui import USE_PYQT5, USE_PYSIDE2
+    if USE_PYSIDE2:
+        from PySide2.QtWidgets import QApplication
+    elif USE_PYQT5:
+        from PyQt5.QtWidgets import QApplication
+    from gui import WedgeScanningWidget
+    app = QApplication( sys.argv )
+    dialog = WedgeScanningWidget( show_scan_button=True )
+    dialog.show()
+    sys.exit( app.exec_() )
+
+if __name__ == "__main__":
+    main()
