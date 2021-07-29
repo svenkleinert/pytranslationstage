8ffd3eced92ee1d014d2a70ff906ae2bfa2814b8
Sven Kleinert <kleinert@iqo.uni-hannover.de>
2021-07-29 19:26:13 +0200

    version changed to 1.0.0

--- pytranslationstage/__init__.py
+++ pytranslationstage/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.0.2"
+__version__ = "1.0.0"
 
 DEBUG = False
 USE_PYQT5 = True
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytranslationstage", # Replace with your own username
-    version="0.0.2",
+    version="1.0.0",
     author="Sven Kleinert",
     author_email="kleinert@iqo.uni-hannover.de",
     description="python translation stage library",
